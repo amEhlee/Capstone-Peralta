cdb4bf8acdb0aebed0fd03a82c6bd888d686bc9e
Elie Kabengele <E.kabe2324@gmail.com>
2022-01-24 20:22:21 -0700

    Inital Commit

---
+++ src/main/java/com/capstone/peralta/PeraltaApplication.java
@@ -0,0 +1,13 @@
+package com.capstone.peralta;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PeraltaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PeraltaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/capstone/peralta/PeraltaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.capstone.peralta;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PeraltaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
