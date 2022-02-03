b1c78bd7830a7e18f8a80a00ea41fe2e7c046b3a
Jared <jared.smith772@gmail.com>
2022-02-03 14:25:33 -0700

    Added basic tests for all service and controller classes
    Should be able to add and get over the API

--- src/main/java/com/capstone/peralta/services/CategoryService.java
+++ src/main/java/com/capstone/peralta/services/CategoryService.java
@@ -1,7 +1,25 @@
 package com.capstone.peralta.services;
 
+import com.capstone.peralta.models.Category;
+import com.capstone.peralta.repos.CategoryRepo;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 @Service
 public class CategoryService {
+
+    private CategoryRepo categoryRepo;
+
+    public Category getCategoryById(int id) {
+        return categoryRepo.getById(id);
+    }
+
+    public List<Category> getAll() {
+        return categoryRepo.findAll();
+    }
+
+    public Category addCategory(Category category) {
+        return categoryRepo.save(category);
+    }
 }
--- src/main/java/com/capstone/peralta/services/OrderService.java
+++ src/main/java/com/capstone/peralta/services/OrderService.java
@@ -1,7 +1,25 @@
 package com.capstone.peralta.services;
 
+import com.capstone.peralta.models.Order;
+import com.capstone.peralta.repos.OrderRepo;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 @Service
 public class OrderService {
+
+    private OrderRepo orderRepo;
+
+    public Order getOrderById(int id) {
+        return orderRepo.getById(id);
+    }
+
+    public List<Order> getAll() {
+        return orderRepo.findAll();
+    }
+
+    public Order addOrder(Order order) {
+        return orderRepo.save(order);
+    }
 }
