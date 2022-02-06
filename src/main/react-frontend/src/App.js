7df632c269f89ba8fd60f394075f939aa9218926
Donnietentoes <donhenrylaliberte@gmail.com>
2022-02-06 00:21:33 -0700

    Optimized Router Flow within App.js using Nested Routes -For explanation visit: https://reactrouter.com/docs/en/v6/getting-started/overview -Allows for better layout wrapping -Also allows future encapsulation  of admin page URL's for minor protection

--- src/main/react-frontend/src/App.js
+++ src/main/react-frontend/src/App.js
@@ -1,17 +1,24 @@
+// noinspection ES6CheckImport
 import { Route, Routes } from 'react-router-dom';
 import Layout from './components/layout/Layout';
 
 import HomePage from './pages/HomePage';
 import LoginPage from './pages/LoginPage';
+import AdminPage from "./pages/AdminPage";
+import AdminLayout from "./components/layout/AdminLayout";
 
 function App() {
   return (
-    <Layout>
       <Routes>
-        <Route path='/' element={<HomePage />} />
-        <Route path='/login' element={<LoginPage />} />
+          <Route path='/' element={<Layout />}>
+            <Route index element={<HomePage />} />
+            <Route path='/login' element={<LoginPage />} />
+          </Route>
+
+          <Route path='/admin' element={<AdminLayout />}>
+              <Route index element={<AdminPage />} />
+          </Route>
       </Routes>
-    </Layout>
   );
 }
 
