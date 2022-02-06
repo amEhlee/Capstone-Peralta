e627fa2c299113be3cc5feb019d60efa18baabff
Elie Kabengele <76502588+amEhlee@users.noreply.github.com>
2022-02-06 15:28:49 -0700

    Resolve App.JS merge conflict
    Messed up this should be the proper app.js

--- src/main/react-frontend/src/App.js
+++ src/main/react-frontend/src/App.js
@@ -3,17 +3,23 @@ import Layout from './components/layout/Layout';
 
 import HomePage from './pages/HomePage';
 import LoginPage from './pages/LoginPage';
-import TestGetComponent from './pages/TestGetComponent';
+import AdminPage from "./pages/AdminPage";
+import TestGetComponent from "./pages/TestGetComponent";
+import AdminLayout from "./components/layout/AdminLayout";
 
 function App() {
   return (
-    <Layout>
       <Routes>
-        <Route path='/' element={<HomePage />} />
-        <Route path='/login' element={<LoginPage />} />
-        <Route path='/TestGet' element={<TestGetComponent />} />
+          <Route path='/' element={<Layout />}>
+            <Route index element={<HomePage />} />
+            <Route path='/login' element={<LoginPage />} />
+            <Route path='/TestGet' element={<TestGetComponent />} />
+          </Route>
+
+          <Route path='/admin' element={<AdminLayout />}>
+              <Route index element={<AdminPage />} />
+          </Route>
       </Routes>
-    </Layout>
   );
 }
 
