2a02bdd8a23e5c44b30d2bd03079a3d46fb765c7
Jeric Geronimo <jericangelo.geronimo@edu.sait.ca>
2022-02-06 23:42:17 -0700

    Added Category choices. Added test page for Cart Icon. Create an IconButtons component for future use. NOTE: CRUD handlers not implemented to IconButtons component, only redirect for now. (Will be updated in the future).

--- src/main/react-frontend/src/App.js
+++ src/main/react-frontend/src/App.js
@@ -1,26 +1,28 @@
-import { Route, Routes } from 'react-router-dom';
+import {Route, Routes} from 'react-router-dom';
 import Layout from './components/layout/Layout';
 
 import HomePage from './pages/HomePage';
 import LoginPage from './pages/LoginPage';
 import AdminPage from "./pages/AdminPage";
+import CartPage from './pages/CartPage';
 import TestGetComponent from "./pages/TestGetComponent";
 import AdminLayout from "./components/layout/AdminLayout";
 
 function App() {
-  return (
-      <Routes>
-          <Route path='/' element={<Layout />}>
-            <Route index element={<HomePage />} />
-            <Route path='/login' element={<LoginPage />} />
-            <Route path='/TestGet' element={<TestGetComponent />} />
-          </Route>
+    return (
+        <Routes>
+            <Route path='/' element={<Layout/>}>
+                <Route index element={<HomePage/>}/>
+                <Route path='/cart' element={<CartPage/>}/>
+                <Route path='/login' element={<LoginPage/>}/>
+                <Route path='/TestGet' element={<TestGetComponent/>}/>
+            </Route>
 
-          <Route path='/admin' element={<AdminLayout />}>
-              <Route index element={<AdminPage />} />
-          </Route>
-      </Routes>
-  );
+            <Route path='/admin' element={<AdminLayout/>}>
+                <Route index element={<AdminPage/>}/>
+            </Route>
+        </Routes>
+    );
 }
 
 export default App;
--- src/main/react-frontend/src/components/layout/NavBar.js
+++ src/main/react-frontend/src/components/layout/NavBar.js
@@ -1,36 +1,28 @@
-import { Link } from 'react-router-dom';
+import IconButtons from './IconButtons';
+import CategoryNav from './CategoryNav';
 import style from './NavBar.module.css';
 
 function NavBar() {
     return (
-        <header className={style.header}>
-            <link href="https://fonts.googleapis.com/icon?family=Material+Icons"
-                rel="stylesheet"></link>
-            <div className={style.logo}>
-                <i class="material-icons">language</i> Peralta Shop
-            </div>
-            {/* Search Functionality will be added in future update */}
-            <div className={style.searchwrapper}>
-                <input type="text" placeholder="Search" className={style.searchBar}></input>
-                <i class="material-icons">search</i>
-            </div>
-            {/* Icon functionality will be added in future update */}
-            <div className={style.icon}>
-                <button>
-                    <i class="material-icons">notifications</i>
-                </button>
-            </div>
-            <div className={style.icon}>
-                <button>
-                    <i class="material-icons">account_circle</i>
-                </button>
-            </div>
-            <div className={style.icon}>
-                <button>
-                    <i class="material-icons">shopping_cart</i>
-                </button>
-            </div>
-        </header>
+        <div>
+            <header className={style.header}>
+                {/* Use material icon library from Google Material */}
+                <link href="https://fonts.googleapis.com/icon?family=Material+Icons"
+                      rel="stylesheet"></link>
+                <div className={style.logo}>
+                    <i class="material-icons">language</i> Peralta Shop
+                </div>
+                {/* Search Functionality will be added in future update */}
+                <div className={style.searchwrapper}>
+                    <input type="text" placeholder="Search" className={style.searchBar}></input>
+                    <i class="material-icons">search</i>
+                </div>
+                <IconButtons iconName="notifications" redirectURL="/"/>
+                <IconButtons iconName="account_circle" redirectURL="/"/>
+                <IconButtons iconName="shopping_cart" redirectURL="/cart"/>
+            </header>
+            <CategoryNav/>
+        </div>
     );
 }
 
