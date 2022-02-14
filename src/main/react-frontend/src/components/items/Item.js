addd8d91ebd2de2aeb2bcfd5e7372f9aad4dfaca
Jeric Geronimo <84466984+JericGeronimo@users.noreply.github.com>
2022-02-14 12:02:59 -0700

    Added CSS styling to Item

--- src/main/react-frontend/src/components/items/Item.js
+++ src/main/react-frontend/src/components/items/Item.js
@@ -1,14 +1,15 @@
 import React from "react";
+import style from './ItemCardLayout.module.css'
 
 export default function Item(props) {
   return (
-    <div>
+    <div className={style.card}>
       <li>
         <h3>{props.name}</h3>
         <p>Item ID:{props.id}</p>
         <p>Price ${props.price}</p>
         <p>Quantity: {props.quantity}</p>
-        <button>Buy Item</button>
+        <button>Add To Cart</button>
       </li>
     </div>
   );
