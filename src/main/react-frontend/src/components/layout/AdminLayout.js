c9e94aec84a32edbb3fa1051fe5c7337961187b1
Jeric Geronimo <84466984+JericGeronimo@users.noreply.github.com>
2022-02-14 16:33:29 -0700

    Added Admin Sidebar Navigation.

--- src/main/react-frontend/src/components/layout/AdminLayout.js
+++ src/main/react-frontend/src/components/layout/AdminLayout.js
@@ -1,10 +1,12 @@
 import style from './Layout.module.css';
 import NavBar from './NavBar';
 import { Outlet } from "react-router-dom";
+import Sidebar from "./Sidebar";
 
 function AdminLayout() {
     return (
         <div>
+            <Sidebar></Sidebar>
             <main className={style.main}><Outlet> </Outlet></main>
         </div>
     );
