b87a89336566ea77908532490687091d868bd606
nicktan-work <75344013+nicktan-work@users.noreply.github.com>
2022-02-16 16:47:59 -0700

    tried to add categories to the dynamically populate in add items, doesnt work as of right now
    CHECKBOXES ARE PRESENT BUT DONT DO ANYTHING AS OF RIGHT NOW

--- src/main/react-frontend/src/components/InventoryManagement/AddItem.js
+++ src/main/react-frontend/src/components/InventoryManagement/AddItem.js
@@ -1,26 +1,80 @@
 import React from "react";
-import { useRef } from "react";
-import { Form, FormGroup, InputGroup, Button } from "react-bootstrap";
+import {useRef, useState, useEffect} from "react";
+import {Form, FormGroup, InputGroup, Button, FormControl} from "react-bootstrap";
 import axios from "axios";
 
+import Item from "../items/Item";
+
 export default function AddItem() {
+    //grabs the categories
+    var [categoryjson, setcategoryjson] = useState([]);
+    const FETCH_URL = "http://localhost:8080/category/all";
+
+    //to check the state of checkbox for each category also updates the state n stuff
+    const [checkedState, setCheckedState] = useState(
+        new Array(categoryjson.length).fill(false)
+    );
+
     const itemNameRef = useRef();
     const itemPriceRef = useRef();
     const itemWeightRef = useRef();
     const itemVolumeRef = useRef();
     const itemQuantityRef = useRef();
     const itemAvailableRef = useRef();
-    const imageRef = useRef();
-    let formData = new FormData();
 
-    function submitHandler() {
+    //initializing array of checked categories
+    const checkedCategories = [];
+
+
+    function getCategories() {
+        return axios
+            .get(FETCH_URL) // preform get request
+            .then((res) => {
+                return res.data; // return response
+            })
+            .catch((err) => console.error(err));
+    }
+
+    useEffect(() => {
+        getCategories().then((data) => {
+            console.log(data); // log returned data
+
+            setcategoryjson(data || "no data returned"); // store returned data in a variable
+        });
+    }, []);
+
+    console.log(categoryjson);
+
+
+    // const handleOnChange = (position) => {
+    //     const updatedCheckedState = checkedState.map((item, index) =>
+    //         index === position ? !item : item
+    //     );
+    //
+    //     setCheckedState(updatedCheckedState);
+    // }
+
+
+    function submitHandler(event) {
+        //event.preventDefault();
         const returnedName = itemNameRef.current.value;
         const returnedPrice = itemPriceRef.current.value;
         const returnedWeight = itemWeightRef.current.value;
         const returnedVolume = itemVolumeRef.current.value;
         const returnedQuantity = itemQuantityRef.current.value;
         let returnedAvailable = itemAvailableRef.current.checked;
-        const imageData = imageRef.current;
+
+        //parses through the checked categories and adds the category_id to an array
+        // const itemCategories = updatedCheckedState.reduce(
+        //     (currentState, index) => {
+        //         if(currentState === true){
+        //             checkedCategories.push(categoryjson[index].category_id);
+        //         }
+        //     }
+        // )
+
+        // const returnedItemCategories = checkedCategories;
+
 
         // parse checkbox result e.g if checkbox_clicked true = 1 if checkbox_clicked false = 0
         returnedAvailable === true ? (returnedAvailable = 1) : (returnedAvailable = 0);
@@ -36,23 +90,16 @@ export default function AddItem() {
             itemWeight: returnedWeight,
         };
 
+
         const POST_URL = "http://localhost:8080/item/add"; // fetch url
         axios.post(POST_URL, item).then((res) => {
-            console.log(res);
-        });
-
-        const UPLOAD_URL = "http://localhost:8080/upload";
-        formData.append('image', imageData.files[0]);
-        axios.post(UPLOAD_URL, formData, {
-            headers: {
-                "Content-Type": "multipart/form-data"
-            }
-        }).then((res) => {
             console.log(res);
         });
     }
 
     return (
+
+
         <Form onSubmit={submitHandler}>
             <FormGroup className="mb-3" controlId="formItemName">
                 <Form.Label>Item Name</Form.Label>
@@ -63,12 +110,10 @@ export default function AddItem() {
                 />
             </FormGroup>
 
-            <FormGroup controlId="formFile" className="mb-3">
-                <Form.Label>Item Image</Form.Label>
-                <Form.Control type="file"
-                    ref={imageRef}
-                />
-            </FormGroup>
+            <Form.Group controlId="formFileMultiple" className="mb-3">
+                <Form.Label>Item Images</Form.Label>
+                <Form.Control type="file" multiple/>
+            </Form.Group>
 
             <FormGroup className="mb-3" controlId="formItemDescription">
                 <Form.Label>Item Description</Form.Label>
@@ -81,15 +126,26 @@ export default function AddItem() {
 
             <FormGroup className="mb-3" controlId="formItemCategory">
                 {/* checkbox */}
+
                 <Form.Label>Item Category</Form.Label>
-                <Form.Check type="checkbox" label="Food" />
-                <Form.Check type="checkbox" label="Clothing" />
-                <Form.Check type="checkbox" label="Electronics" />
+
+                {categoryjson.map((i) => (
+                    <Form.Check type="checkbox" name="category" value={i.category_id} label={i.categoryName}
+                                // checked={checkedState[index]} onChange={handleOnChange(index)}  <- attempted to put items in array
+                    />
+                ))}
+
+
+                {/*the following will allow you to create a checkbox with custom text....*/}
+                {/*<InputGroup className="mb-3">*/}
+                {/*    <InputGroup.Checkbox aria-label="Checkbox for following text input" />*/}
+                {/*    <FormControl aria-label="Text input with checkbox" />*/}
+                {/*</InputGroup>*/}
             </FormGroup>
 
             <FormGroup className="mb-3" controlId="formItemAvailable">
                 <Form.Label>Available</Form.Label>
-                <Form.Check type="checkbox" label="Available" ref={itemAvailableRef} />
+                <Form.Check type="checkbox" label="Available" ref={itemAvailableRef}/>
             </FormGroup>
 
             <FormGroup className="mb-3" controlId="formItemPrice">
@@ -134,6 +190,8 @@ export default function AddItem() {
             <Button type="submit" className="btn btn-success">
                 Add Item
             </Button>
+
+            <Button className="btn btn-success" onClick={getCategories}>console </Button>
         </Form>
     );
 }
