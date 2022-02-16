921ef712a345ce5c3bf16677a1756248ecd760f9
Donnietentoes <donhenrylaliberte@gmail.com>
2022-02-16 02:47:05 -0700

    -Added Availability Checkboxes on Manage Items page

--- src/main/react-frontend/src/components/items/ManageItems.js
+++ src/main/react-frontend/src/components/items/ManageItems.js
@@ -4,20 +4,19 @@ import { Button } from 'react-bootstrap';
 
 export default function Item(props) {
 
-
-  return (
+    return (
         <tr>
-          <td>{props.id}</td>
-          <td>{props.name}</td>
-          <td>${props.price}</td>
-          <td>{props.weight}kg</td>
-          <td>{props.quantity}</td>
-          <td>{props.available}</td>
-          <td>
-            <Link to={"/admin/editItem"}>
-              <Button variant="primary" size="sm">Edit</Button>
-            </Link>
-          </td>
+            <td>{props.id}</td>
+            <td>{props.name}</td>
+            <td>${props.price}</td>
+            <td>{props.weight}kg</td>
+            <td>{props.quantity}</td>
+            <td><input type="checkbox" checked={props.available} readOnly={"readonly"} /></td>
+            <td>
+                <Link to={"/admin/editItem"}>
+                    <Button variant="primary" size="sm">Edit</Button>
+                </Link>
+            </td>
         </tr>
-  );
+    );
 }
