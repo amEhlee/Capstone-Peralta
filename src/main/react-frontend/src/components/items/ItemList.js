3d42f4c24c67c027be4534d6b5780f2ea4d38ca7
Elie Kabengele <Elie.Kabengele@edu.sait.ca>
2022-03-29 16:33:39 -0600

    cleanup after deleting cartItems.js

--- src/main/react-frontend/src/components/items/ItemList.js
+++ src/main/react-frontend/src/components/items/ItemList.js
@@ -1,94 +1,67 @@
 // Import Dependencies
-import React from 'react';
+import React from "react";
 
 // Import Components
-import Item from './Item';
-import ManageItemsTable from './ManageItemsTable';
+import Item from "./Item";
+import ManageItemsTable from "./ManageItemsTable";
 
 // Import Styles
-import style from '../../assets/styles/ItemCardLayout.module.css'
-import CartItems from './CartItems';
+import style from "../../assets/styles/ItemCardLayout.module.css";
 
 export default function ItemList(props) {
+	function regularList() {
+		return (
+			<ul className={style.unorderedList}>
+				{props.items.map((i) => (
+					<Item
+						key={i.itemId}
+						id={i.itemId}
+						name={i.itemName}
+						price={i.itemPrice}
+						weight={i.itemWeight}
+						volume={i.itemVolume}
+						quantity={i.itemQuantity}
+						available={i.itemAvailable}
+					/>
+				))}
+			</ul>
+		);
+	}
 
-  
-  function regularList() {
-    return (
-      <ul className={style.unorderedList}>
-        {props.items.map((i) => (
-          <Item
-          key={i.itemId}
-          id={i.itemId}
-          name={i.itemName}
-          price={i.itemPrice}
-          weight={i.itemWeight}
-            volume={i.itemVolume}
-            quantity={i.itemQuantity}
-            available={i.itemAvailable}
-          />
-        ))}
-      </ul>
-    );
-  }
-  
-  function adminList() {
-    return (
-      <tbody>
-        {props.items.filter((val) => {
-          if (props.search == "") {
-            return val;
-          } else if (val.itemName.toLowerCase().includes(props.search.toLowerCase())) {
-            return val;
-          }
-        }
-        ).map((i) => (
-          <ManageItemsTable
-          key={i.itemId}
-          id={i.itemId}
-          name={i.itemName}
-            category={i.category}
-            price={i.itemPrice}
-            weight={i.itemWeight}
-            volume={i.itemVolume}
-            quantity={i.itemQuantity}
-            available={i.itemAvailable}
-            item={i}
-            />
-            ))}
-      </tbody>
-    );
-  }
+	function adminList() {
+		return (
+			<tbody>
+				{props.items
+					.filter((val) => {
+						if (props.search == "") {
+							return val;
+						} else if (
+							val.itemName.toLowerCase().includes(props.search.toLowerCase())
+						) {
+							return val;
+						}
+					})
+					.map((i) => (
+						<ManageItemsTable
+							key={i.itemId}
+							id={i.itemId}
+							name={i.itemName}
+							category={i.category}
+							price={i.itemPrice}
+							weight={i.itemWeight}
+							volume={i.itemVolume}
+							quantity={i.itemQuantity}
+							available={i.itemAvailable}
+							item={i}
+						/>
+					))}
+			</tbody>
+		);
+	}
 
-  // const counts = {};
-  // const sampleArray = ['a', 'a', 'b', 'c'];
-  // sampleArray.forEach(function (x) { counts[x] = (counts[x] || 0) + 1; });
-  // console.log(counts)
-
-  function cartList() {
-    // const counts = {};
-    // props.items.map((i) => (
-    //     counts[i] = (counts[i] || 0) + 1
-    // ));
-    // console.log(counts);
-    return (
-      <tbody>
-        {props.items.map((i) => (
-          <CartItems
-            key={i.itemId}
-            itemName={i.itemName}
-            itemQuantity={i.itemQuantity}
-            itemPrice={i.itemPrice}
-          />
-        ))}
-      </tbody>
-    )
-  }
-
-  if(props.target === "regularList") {
-    return regularList();
-  } else if (props.target === "adminList" ){
-    return adminList();
-  } else if (props.target === "cartList") {
-    return cartList();
-  }
+	if (props.target === "regularList") {
+		return regularList();
+	} else if (props.target === "adminList") {
+		return adminList();
+	}
 }
