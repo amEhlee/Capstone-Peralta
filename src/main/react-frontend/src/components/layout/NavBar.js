d845a14146c18788a32b8bf97d335246f24198c8
nicktan-work <75344013+nicktan-work@users.noreply.github.com>
2022-04-01 14:04:52 -0600

    Cleaned up NavBar.js, Removed HomePage title from HomePage.js

--- src/main/react-frontend/src/components/layout/NavBar.js
+++ src/main/react-frontend/src/components/layout/NavBar.js
@@ -32,19 +32,29 @@ function NavBar() {
             <div>
                 <Navbar bg="light" variant="light">
                     <Container>
-                        <button onClick={() => {navigate("/")}}>
+                        <button onClick={() => {
+                            navigate("/")
+                        }}>
                             <Navbar.Brand><BsShop/> Peralta Shop</Navbar.Brand>
                         </button>
-                        <Nav>
-                            {/*TODO: ADD NAVIGATION*/}
-                            <NavDropdown title="Navigation">
-                                <NavDropdown.Item onClick={() => {navigate("/")}}>Home</NavDropdown.Item>
-                                <NavDropdown.Item onClick={() => {navigate("/order")}}>Orders</NavDropdown.Item>
-                                <NavDropdown.Item onClick={() => {navigate("/userProfile")}}>User Profile</NavDropdown.Item>
-                                <NavDropdown.Item onClick={() => {navigate("/admin/manageItems")}}>Admin Side</NavDropdown.Item>
-                            </NavDropdown>
-                        </Nav>
-                        {/* TODO: add search functionality */}
+                        {/*<Nav>*/}
+                        {/*    /!*TODO: ADD NAVIGATION*!/*/}
+                        {/*    <NavDropdown title="Navigation">*/}
+                        {/*        <NavDropdown.Item onClick={() => {*/}
+                        {/*            navigate("/")*/}
+                        {/*        }}>Home</NavDropdown.Item>*/}
+                        {/*        <NavDropdown.Item onClick={() => {*/}
+                        {/*            navigate("/order")*/}
+                        {/*        }}>Orders</NavDropdown.Item>*/}
+                        {/*        <NavDropdown.Item onClick={() => {*/}
+                        {/*            navigate("/userProfile")*/}
+                        {/*        }}>User Profile</NavDropdown.Item>*/}
+                        {/*        <NavDropdown.Item onClick={() => {*/}
+                        {/*            navigate("/admin/manageItems")*/}
+                        {/*        }}>Admin Side</NavDropdown.Item>*/}
+                        {/*    </NavDropdown>*/}
+                        {/*</Nav>*/}
+                        {/* TODO: add search functionaliy */}
                         <div className={style.searchwrapper}>
                             <input
                                 type="text"
@@ -54,35 +64,80 @@ function NavBar() {
                             <BsSearch/>
                         </div>
 
-                        <IconButtons
-                            iconElement={BsFillBellFill()}
-                            buttonVariant="light"
-                            redirectURL="/"
-                        />
 
-                        <Link to="/login">
+                        {/*NOT USEFUL RN*/}
+                        {/*<IconButtons*/}
+                        {/*    iconElement={BsFillBellFill()}*/}
+                        {/*    buttonVariant="light"*/}
+                        {/*    redirectURL="/"*/}
+                        {/*/>*/}
+
+
+                        {/*<NavDropdown title="Account">*/}
+                        {/*    <NavDropdown.Item>Order History</NavDropdown.Item>*/}
+                        {/*    <NavDropdown.Item>Edit Account</NavDropdown.Item>*/}
+                        {/*    <Link to="/login"><NavDropdown.Item href="/login">Log In</NavDropdown.Item></Link>*/}
+                        {/*    <NavDropdown.Item>Log out</NavDropdown.Item>*/}
+
+
+                        {/*</NavDropdown>*/}
+
+                        <Nav>
+                            {/*TODO: ADD NAVIGATION*/}
+                            <NavDropdown title="Account">
+                                <NavDropdown.Item onClick={() => {
+                                    navigate("/order")
+                                }}>Order History</NavDropdown.Item>
+                                <NavDropdown.Item onClick={() => {
+                                    navigate("/signup")
+                                }}>Sign Up</NavDropdown.Item>
+                                <NavDropdown.Item onClick={() => {
+                                    navigate("/userProfile")
+                                }}>Edit Profile</NavDropdown.Item>
+                                <NavDropdown.Item onClick={() => {
+                                    navigate("/login")
+                                }}>Login</NavDropdown.Item>
+                            </NavDropdown>
+                        </Nav>
+
+
+
+                        {/*REDIRECTS TO ADMIN PAGE NOW*/}
+                        <Link to="/admin">
                             <Button variant="light" onClick={handleShow}>
                                 {BsFillPersonFill()}
+                                <div>Admin</div>
                             </Button>
                         </Link>
 
-                        <IconButtons
-                            iconElement={BsFillCartFill()}
-                            buttonVariant="light"
-                            redirectURL="/cart"
-                        />
+
+                        {/*TODO: ADD AMOUNT OF ITEMS IN CARD TO THE NAVBAR*/}
+                        <Link to="/cart">
+                            <Button variant="light" onClick={handleShow}>
+                                {BsFillCartFill()}
+                                <div>Cart</div>
+                            </Button>
+                        </Link>
+
+                        {/*<IconButtons*/}
+                        {/*    iconElement={BsFillCartFill()}*/}
+
+                        {/*    buttonVariant="light"*/}
+                        {/*    redirectURL="/cart"*/}
+                        {/*/>*/}
+
                     </Container>
                 </Navbar>
                 {/*<CategoryNav/>*/}
             </div>
-            <Modal show={show} onHide={handleClose}>
-                <Modal.Header closeButton>
-                    <Modal.Title>Login</Modal.Title>
-                </Modal.Header>
-                <Modal.Body>
-                    <LoginForm/>
-                </Modal.Body>
-            </Modal>
+            {/*<Modal show={show} onHide={handleClose}>*/}
+            {/*    <Modal.Header closeButton>*/}
+            {/*        <Modal.Title>Login</Modal.Title>*/}
+            {/*    </Modal.Header>*/}
+            {/*    <Modal.Body>*/}
+            {/*        <LoginForm/>*/}
+            {/*    </Modal.Body>*/}
+            {/*</Modal>*/}
         </>
     );
 }
