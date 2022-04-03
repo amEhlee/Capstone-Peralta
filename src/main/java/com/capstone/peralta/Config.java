a8fcf0cd8139f956c1179fcad3d5bb2577aabccb
Donnietentoes <donhenrylaliberte@gmail.com>
2022-04-03 16:17:33 -0600

    Fixing Signup

---
+++ src/main/java/com/capstone/peralta/Config.java
@@ -0,0 +1,15 @@
+package com.capstone.peralta;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.mail.javamail.JavaMailSenderImpl;
+
+@Configuration
+public class Config {
+
+    @Bean
+    public JavaMailSender javaMailSender() {
+        return new JavaMailSenderImpl();
+    }
+}
