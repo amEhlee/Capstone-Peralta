30e47bb942ca7979f65c146d1cff66bd6889cd7c
AmEhlee <E.Kabe2324@gmail.com>
2022-04-05 13:21:17 -0600

    Dynamically Render UserButton - if signed in as admin button goes to adminpage - if signed in as user button goes to profile page - if signed in as guest button goes to login page - changed default profile information to point to user data - changed editinfromation to point to user data - added logout functionality to profile - go back to homepage on profile and editprofile if not logged in

--- src/main/react-frontend/src/components/UserSide/UserProfile.js
+++ src/main/react-frontend/src/components/UserSide/UserProfile.js
@@ -1,54 +1,67 @@
-
-import React, {useRef, useState, useEffect, useContext} from "react";
-import axios from "axios";
-import {UserContext} from "../../UserContext";
+import React, { useContext } from "react";
+import { UserContext } from "../../UserContext";
 
 // Import Components
-import {Form, FormGroup, InputGroup, Button, FormControl, Col, Row} from "react-bootstrap";
-import {useNavigate} from "react-router-dom";
+import { Form, FormGroup, Button } from "react-bootstrap";
+import { Link, Navigate } from "react-router-dom";
 import Style from "../../assets/styles/UserSide.module.css";
 
-
 export default function UserProfile() {
+	const userContext = useContext(UserContext).contextData.user;
 
+	// check if user is null if so rendirec to home
+	if (!userContext) {
+		return <Navigate to="/" />
+	} else {
+		return (
+			<>
+				<div>
+					<Form className={Style.centrize}>
+						<h1>Personal Details</h1>
+						<FormGroup className="mb-3" controlId="formFname">
+							<Form.Label> First Name: </Form.Label>
+							<Form.Control
+								type="text"
+								placeholder="Enter your first name"
+								disabled
+								defaultValue={userContext.firstName}
+							/>
+						</FormGroup>
 
-    const userContext = useContext(UserContext).contextData.user;
-    const navigate = useNavigate();
+						<Form.Group controlId="formLname" className="mb-3">
+							<Form.Label>Last Name: </Form.Label>
+							<Form.Control
+								type="text"
+								placeholder="Enter your last name"
+								disabled
+								defaultValue={userContext.lastName}
+							/>
+						</Form.Group>
 
+						<FormGroup className="mb-3" controlId="formEmail">
+							<Form.Label>Email: </Form.Label>
+							<Form.Control
+								type="text"
+								placeholder="Enter your Email"
+								disabled
+								defaultValue={userContext.email}
+							/>
+						</FormGroup>
 
-    return (
-        <Form className={Style.centrize}>
-            <h1>Personal Details</h1>
-            <FormGroup className="mb-3" controlId="formFname">
-                <Form.Label> First Name: </Form.Label>
-                <Form.Control
-                    type="text"
-                    placeholder="Enter your first name" disabled
-                    defaultValue={userContext.userFirstName}
-                />
-            </FormGroup>
+						<Link to="/editProfile">
+							<Button type="submit" className="btn btn-success">
+								Edit Account
+							</Button>
+						</Link>
 
-            <Form.Group controlId="formLname" className="mb-3">
-                <Form.Label>Last Name: </Form.Label>
-                <Form.Control type="text"
-                              placeholder="Enter your last name" disabled
-                              defaultValue={userContext.userLastName}
-                />
-            </Form.Group>
-
-            <FormGroup className="mb-3" controlId="formEmail">
-                <Form.Label>Email: </Form.Label>
-                <Form.Control
-                    type="text"
-                    placeholder="Enter your Email" disabled
-                    defaultValue={userContext.email}
-                />
-            </FormGroup>
-
-            <Button type="submit" className="btn btn-success" onClick={() => {navigate("/editProfile")}}>
-                Edit Account
-            </Button>
-
-</Form>
-            )
+						<Link to="/logout">
+							<Button variant="primary" className="btn btn-warning">
+								Log out
+							</Button>
+						</Link>
+					</Form>
+				</div>
+			</>
+		);
+	}
 }
