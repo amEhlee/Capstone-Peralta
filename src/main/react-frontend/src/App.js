2ac7aa3ce00ea252606680b5d98239dea4da14aa
Sanazi-8 <sanazi1381@gmail.com>
2022-04-05 14:02:43 -0600

    - added the url to reach orders page - Basic front end layout should be there - css needed to look nicer for both checkout and order don worry bout it

--- src/main/react-frontend/src/App.js
+++ src/main/react-frontend/src/App.js
@@ -1,5 +1,6 @@
+
 // Import Components
-import { Route, Routes, Navigate } from "react-router-dom";
+import { Route, Routes } from "react-router-dom";
 import { UserProvider, UserContext } from "./UserContext";
 import Layout from "./components/layout/Layout";
 import HomePage from "./pages/HomePage";
@@ -11,20 +12,22 @@ import AddItem from "./components/items/AddItem";
 import EditItem from "./components/items/EditItem";
 import ManageItemsPage from "./pages/ManageItemsPage";
 
-import RequireAuth from "./RequireAuth";
-
 import LoginUserPage from "./pages/LoginUserPage";
-import LogoutUserPage from "./pages/LogoutUserPage";
 import SignUpUserPage from "./pages/SignUpUserPage";
-import Checkout from "./components/UserSide/CheckoutItem";
+import Checkout from "./components/UserSide/CheckoutItem"
 
 import AddCategory from "./components/categories/AddCategory";
 import SelectCategory from "./components/categories/SelectCategory";
 import ManageUsersPage from "./pages/ManageUsersPage";
 
+
+
 import ProductPage from "./pages/ProductPage";
 import UserProfile from "./components/UserSide/UserProfile";
 import EditProfile from "./components/UserSide/EditProfile";
+
+
+
 
 // Import Styling
 import "bootstrap/dist/css/bootstrap.min.css";
@@ -47,24 +50,27 @@ export default function App() {
 					<Route path="/item/:itemid" element={<ProductPage />} />
 					<Route path="search/:searchTerm" element={<SearchPage />} />
 
-					<Route path="/userProfile" element={<UserProfile />} />
-					<Route path="/checkout" element={<Checkout />} />
+					<Route path="/userProfile" element={<UserProfile/>}/>
+					<Route path="/checkout" element={<Checkout/>}/>
 				</Route>
 
-				<Route path="/logout" element={<LogoutUserPage />} />
 				<Route path="/login" element={<LoginUserPage />} />
 				<Route path="/signUp" element={<SignUpUserPage />} />
-				<Route path="/editProfile" element={<EditProfile />} />
+				<Route path="/editProfile" element={<EditProfile/>}/>
 
-				<Route exact path="/admin" element={<RequireAuth />}>
-					{/* Example Path Would Be e.g /admin/inventory */}
-					<Route exact path="/admin" element={<AdminLayout />}>
-						<Route index element={<AdminPage />} />
-						<Route path="manageItems" element={<ManageItemsPage />} />
-						<Route path="addCategory" element={<AddCategory />} />
-						<Route path="selectCategory" element={<SelectCategory />} />
-						<Route path="manageUsers" element={<ManageUsersPage />} />
-					</Route>
+
+
+				{/* Example Path Would Be e.g /admin/inventory */}
+				<Route path="/admin" element={<AdminLayout />}>
+					<Route index element={<AdminPage />} />
+					<Route path="addItem" element={<AddItem />} />{" "}
+					{/* TODO remove path?*/}
+					<Route path="editItem" element={<EditItem />} />{" "}
+					{/* TODO remove path?*/}
+					<Route path="manageItems" element={<ManageItemsPage />} />
+					<Route path="addCategory" element={<AddCategory />} />
+					<Route path="selectCategory" element={<SelectCategory />} />
+					<Route path="manageUsers" element={<ManageUsersPage />} />
 				</Route>
 			</Routes>
 		</>
