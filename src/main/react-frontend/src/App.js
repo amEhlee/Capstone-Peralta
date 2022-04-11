7f354d94c13d915734d3e9b202540c7951a63079
AmEhlee <E.Kabe2324@gmail.com>
2022-04-10 18:36:26 -0600

    resolve admin redirects and fix roles issue on editprofile

--- src/main/react-frontend/src/App.js
+++ src/main/react-frontend/src/App.js
@@ -1,37 +1,36 @@
-
 // Import Components
 import { Route, Routes } from "react-router-dom";
-import { UserProvider, UserContext } from "./UserContext";
+import { UserContext } from "./UserContext";
+
+// import general ui
 import Layout from "./components/layout/Layout";
-import HomePage from "./pages/HomePage";
+
+// import admin pages
+import RequireAuth from "./RequireAuth"
 import AdminPage from "./pages/AdminPage";
-import CartPage from "./pages/CartPage";
 import ViewAllItemsPage from "./pages/ViewAllItemsPage";
 import AdminLayout from "./components/layout/AdminLayout";
-import AddItem from "./components/items/AddItem";
-import EditItem from "./components/items/EditItem";
 import ManageItemsPage from "./pages/ManageItemsPage";
 import ManageUsersPage from "./pages/ManageUsersPage";
 import ManageOrdersPage from "./pages/ManageOrdersPage";
+import AddCategory from "./components/categories/AddCategory";
+import SelectCategory from "./components/categories/SelectCategory";
 
+// import user pages
+import HomePage from "./pages/HomePage";
 import LoginUserPage from "./pages/LoginUserPage";
 import LogoutUserPage from "./pages/LogoutUserPage";
 import SignUpUserPage from "./pages/SignUpUserPage";
 import CheckoutPage from "./pages/CheckoutPage";
-
-import AddCategory from "./components/categories/AddCategory";
-import SelectCategory from "./components/categories/SelectCategory";
-
+import CartPage from "./pages/CartPage";
 import ProductPage from "./pages/ProductPage";
 import UserProfile from "./components/users/UserProfile";
 import EditProfile from "./components/users/EditProfile";
 import OrderUser from "./components/orders/Order";
-
-
+import SearchPage from "./pages/SearchPage";
 
 // Import Styling
 import "bootstrap/dist/css/bootstrap.min.css";
-import SearchPage from "./pages/SearchPage";
 //import OrderHistoryPage from "./pages/OrderHistoryPage"; no export default this breaks stuff
 
 export default function App() {
@@ -53,26 +52,26 @@ export default function App() {
 					{/* fix actual exported function first please */}
 					{/* <Route path="/orderhistory" element={<OrderHistoryPage />} /> */}
 
-					<Route path="/userProfile" element={<UserProfile/>}/>
-					<Route path="/checkout" element={<CheckoutPage/>}/>
+					<Route path="/userProfile" element={<UserProfile />} />
+					<Route path="/checkout" element={<CheckoutPage />} />
 				</Route>
 
 				<Route path="/order" element={<OrderUser />} />
 				<Route path="/login" element={<LoginUserPage />} />
 				<Route path="/logout" element={<LogoutUserPage />} />
 				<Route path="/signUp" element={<SignUpUserPage />} />
-				<Route path="/editProfile" element={<EditProfile/>}/>
-
-
+				<Route path="/editProfile" element={<EditProfile />} />
 
 				{/* Example Path Would Be e.g /admin/inventory */}
-				<Route path="/admin" element={<AdminLayout />}>
-					<Route index element={<AdminPage />} />
-					<Route path="manageItems" element={<ManageItemsPage />} />
-					<Route path="manageOrders" element={<ManageOrdersPage />} />
-					<Route path="manageUsers" element={<ManageUsersPage />} />
-					<Route path="addCategory" element={<AddCategory />} />
-					<Route path="selectCategory" element={<SelectCategory />} />
+				<Route exact path="/admin" element={<RequireAuth />}>
+					<Route path="/admin" element={<AdminLayout />}>
+						<Route index element={<AdminPage />} />
+						<Route path="manageItems" element={<ManageItemsPage />} />
+						<Route path="manageOrders" element={<ManageOrdersPage />} />
+						<Route path="manageUsers" element={<ManageUsersPage />} />
+						<Route path="addCategory" element={<AddCategory />} />
+						<Route path="selectCategory" element={<SelectCategory />} />
+					</Route>
 				</Route>
 			</Routes>
 		</>
--- src/main/react-frontend/src/components/users/EditProfile.js
+++ src/main/react-frontend/src/components/users/EditProfile.js
@@ -78,6 +78,7 @@ export default function EditProfile() {
 			phoneNumber: returnedPhone,
 			address: returnedAddress,
 			postalCode: returnedPostalCode,
+			roles: userContext.roles,
 		};
 
 		if(retunedNewPassword !== returnedUserNewPasswordConfirm) {
